4af9832f466e7baff2adfc8fd004be13ccce550b
Ana Felizatti <ana.felizatti@locaweb.com.br>
2022-01-26 11:53:06 -0300

    adjusting css to purple style

--- pages/index.js
+++ pages/index.js
@@ -101,8 +101,8 @@ export default function PaginaInicial() {
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
-            backgroundColor: appConfig.theme.colors.primary[500],
-            backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+            backgroundColor: appConfig.theme.colors.primary[100],
+            backgroundImage: 'url(https://wallpapercave.com/wp/wp7698391.jpg)',
             backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
           }}
         >
@@ -116,9 +116,9 @@ export default function PaginaInicial() {
                 sm: 'row',
               },
               width: '100%', maxWidth: '700px',
-              borderRadius: '5px', padding: '32px', margin: '16px',
-              boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-              backgroundColor: appConfig.theme.colors.neutrals[700],
+              borderRadius: '30px', padding: '50px', margin: '26px',
+              boxShadow: '0 10px 20px 0 rgb(0 0 0 / 40%)',
+              backgroundColor: appConfig.theme.colors.neutrals[400],
             }}
           >
             {/* Formulário */}
@@ -126,11 +126,11 @@ export default function PaginaInicial() {
               as="form"
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
-                width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+                width: { xs: '100%', sm: '60%' }, textAlign: 'center', marginBottom: '10px',
               }}
             >
-              <Title tag="h2">Boas vindas de volta!</Title>
-              <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+              <Title tag="h2">Welcome!</Title>
+              <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[800] }}>
                 {appConfig.name}
               </Text>
   
@@ -138,10 +138,10 @@ export default function PaginaInicial() {
                 fullWidth
                 textFieldColors={{
                   neutral: {
-                    textColor: appConfig.theme.colors.neutrals[200],
-                    mainColor: appConfig.theme.colors.neutrals[900],
-                    mainColorHighlight: appConfig.theme.colors.primary[500],
-                    backgroundColor: appConfig.theme.colors.neutrals[800],
+                    textColor: appConfig.theme.colors.primary[800],
+                    mainColor: appConfig.theme.colors.neutrals[200],
+                    mainColorHighlight: appConfig.theme.colors.primary[900],
+                    backgroundColor: appConfig.theme.colors.neutrals[300],
                   },
                 }}
               />
@@ -151,9 +151,9 @@ export default function PaginaInicial() {
                 fullWidth
                 buttonColors={{
                   contrastColor: appConfig.theme.colors.neutrals["000"],
-                  mainColor: appConfig.theme.colors.primary[500],
-                  mainColorLight: appConfig.theme.colors.primary[400],
-                  mainColorStrong: appConfig.theme.colors.primary[600],
+                  mainColor: appConfig.theme.colors.primary[600],
+                  mainColorLight: appConfig.theme.colors.primary[600],
+                  mainColorStrong: appConfig.theme.colors.primary[800],
                 }}
               />
             </Box>
@@ -170,8 +170,8 @@ export default function PaginaInicial() {
                 padding: '16px',
                 backgroundColor: appConfig.theme.colors.neutrals[800],
                 border: '1px solid',
-                borderColor: appConfig.theme.colors.neutrals[999],
-                borderRadius: '10px',
+                borderColor: appConfig.theme.colors.neutrals[900],
+                borderRadius: '10%',
                 flex: 1,
                 minHeight: '240px',
               }}
@@ -179,17 +179,17 @@ export default function PaginaInicial() {
               <Image
                 styleSheet={{
                   borderRadius: '50%',
-                  marginBottom: '16px',
+                  marginBottom: '15px',
                 }}
                 src={`https://github.com/${username}.png`}
               />
               <Text
                 variant="body4"
                 styleSheet={{
-                  color: appConfig.theme.colors.neutrals[200],
-                  backgroundColor: appConfig.theme.colors.neutrals[900],
-                  padding: '3px 10px',
-                  borderRadius: '1000px'
+                  color: appConfig.theme.colors.neutrals[800],
+                  backgroundColor: appConfig.theme.colors.neutrals[300],
+                  padding: '5px 10px',
+                  borderRadius: '100px'
                 }}
               >
                 {username}
